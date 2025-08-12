54bfebea7b2efa91dfaa8010596c745d3af2b4e7
mervcelik <mrv.ozturrk@gmail.com>
2025-08-12 17:10:43 +0300

    Onion Architecture & CQRS --> Order

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/RemoveAddressCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+public class RemoveAddressCommand
+{
+    public int Id { get; set; }
+
+    public RemoveAddressCommand(int ıd)
+    {
+        Id = ıd;
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandler/GetOrderDetailByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandler;
+
+public class GetOrderDetailByIdQueryHandler
+{
+    private readonly IRepository<OrderDetail> _repository;
+
+    public GetOrderDetailByIdQueryHandler(IRepository<OrderDetail> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+    {
+        var orderDetail = await _repository.GetByIdAsync(query.Id);
+
+        return new GetOrderDetailByIdQueryResult
+        {
+            OrderDetailId = orderDetail.OrderDetailId,
+            ProductId = orderDetail.ProductId,
+            ProductName = orderDetail.ProductName,
+            ProductPrice = orderDetail.ProductPrice,
+            ProductAmount = orderDetail.ProductAmount,
+            ProductTotalPrice = orderDetail.ProductTotalPrice,
+            OrderingId = orderDetail.OrderingId
+        };
+    }
+}
